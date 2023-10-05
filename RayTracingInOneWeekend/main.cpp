ed7c8f6fc5ba71029f9c0e706629e021e034a652
shunya329 <motesyun0329@gmail.com>
2023-10-05 17:18:38 +0900

    attended vec3.h and color.h

--- RayTracingInOneWeekend/main.cpp
+++ RayTracingInOneWeekend/main.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
+// #include <fstream>
+#include "vec3.h"
+#include "color.h"
 
 int main() {
 	const int image_width = 256;
 	const int image_height = 256;
 
+	// out file stream
+	//std::ofstream ofs("image.ppm");
+	//if (!ofs)
+	//{
+	//	std::cout << "failed to open image file\n";
+	//}
+
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	//ofs << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for (int j = image_height - 1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i) {
-			auto r = double(i) / (image_width-1);
-			auto g = double(j) / (image_height - 1);
-			auto b = 0.25;
+			//auto r = double(i) / (image_width - 1);
+			//auto g = double(j) / (image_height - 1);
+			//auto b = 0.25;
 
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
+			//int ir = static_cast<int>(255.999 * r);
+			//int ig = static_cast<int>(255.999 * g);
+			//int ib = static_cast<int>(255.999 * b);
+
+
+			color pixel_color(double(i) / (image_width - 1),
+				double(j) / (image_height - 1),
+				0.25);
+			write_color(std::cout, pixel_color);
 
 			// std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			// ofs << ir << ' ' << ig << ' ' << ib << '\n';
 		}
 	}
+
 	std::cerr << "\nDone.\n";
+
+	//ofs.close();
 }
